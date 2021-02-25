90597dfdac511ba2df3db007f9a975189d839350
jaequan876 <78787406+jaequan876@users.noreply.github.com>
2021-02-25 02:18:43 -0500

    Add files via upload
    P1HW2_BasicMath submission.

---
+++ P1HW2_BasicMath_PerrinJae'Quan.py
@@ -0,0 +1,28 @@
+# Program Psuedocode
+# This program will prompt the user to input two numbers then it will display those numbers then print the sum and the product of both numbers when multipied.
+# February 19,2021
+# CTI-110 P1HW2 - Basic Math
+# Jae'Quan Perrin
+#
+# Prompt user to enter first number
+# The user inputs first number
+# Prompt user to enter second number
+# The user inputs second number
+# The sum of the inputs is calculated
+# The inputs are multiplied together
+# Output first number
+# Output second number
+# Output sum of both numbers
+# Display the result of both numbers when multipied
+
+print('Please Enter the First number:')
+firstnumber = int(input())
+print('Please Enter the Second number:')     
+secondnumber = int(input())
+sum = firstnumber + secondnumber
+product = firstnumber * secondnumber
+print('The first number entered is:', firstnumber)
+print('The second number entered is:', secondnumber)
+print('The sum of both numbers is:', sum)
+print('When both numbers were multiplied the result is:', product)
+
